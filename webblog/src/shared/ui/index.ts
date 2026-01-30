24bc08c3b0e2175bf449f363a4f6eaea0aa38d0c
Ivan Akimov <136077992+ivan41kd@users.noreply.github.com>
2026-01-30 13:47:07 +0300

    Revert "Features/fsd structure"

--- webblog/src/shared/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Button";
-export * from "./Checkbox";
-export * from "./Input";
-export * from "./Text";
-export * from "./Title";
--- webblog/src/shared/ui/types/Text/type.ts
+++
@@ -1,8 +0,0 @@
-import type { FontSizeType, FontWeightType } from "../index";
-
-export interface TextPropsType {
-  text: string;
-  className?: string;
-  fontSize?: FontSizeType;
-  fontWeight?: FontWeightType;
-}
